7c0509e6ce9056d52c230634c20a8584920b552c
supanat_k <supanat_k@tripetch-it.co.th>
2023-05-30 17:20:26 +0700

    refactor state to custom hook

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,42 +1,32 @@
-import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
 import { fetchUsers, addUser } from "../store";
 import Button from "../components/Button";
 import Skeleton from "./Skeleton";
+import { useThunk } from "../hooks/use-thunk";
 
 function UserList() {
-  const [isLoadingUsers, setIsLoadingUsers] = useState(false);
-  const [isLoadingUsersError, setIsLoadingUsersError] = useState(null);
-  const [isCreatingUser, setIsCreatingUser] = useState(false);
-  const [creatingUserError, setCreatingUserError] = useState(null);
-  const dispatch = useDispatch();
+  const [doFetchUsers, isLoadingUsers, loadingUsersError] =
+    useThunk(fetchUsers);
+  const [doCreateUser, isCreatingUser, creatingUserError] = useThunk(addUser);
+
   const { data } = useSelector((state) => {
     return state.users;
   });
 
   useEffect(() => {
-    setIsLoadingUsers(true);
-    dispatch(fetchUsers())
-      .unwrap() // createAsyncThunk จะ return เป็น promise resolve เสมอดังนั้นถ้าไม่ใช้อันนี้จะไม่มีทางไปถึง catch
-      .catch((err) => setIsLoadingUsersError(err))
-      .finally(() => {
-        setIsLoadingUsers(false);
-      });
-  }, [dispatch]);
+    doFetchUsers();
+  }, [doFetchUsers]);
 
   const handleUserAdd = () => {
-    setIsCreatingUser(true);
-    dispatch(addUser())
-      .unwrap()
-      .catch((err) => setCreatingUserError(err))
-      .finally(() => setIsCreatingUser(false));
+    doCreateUser();
   };
 
   if (isLoadingUsers) {
     return <Skeleton times={6} className="h-10 w-full" />;
   }
 
-  if (isLoadingUsersError) {
+  if (loadingUsersError) {
     return <div>Error fetching data...</div>;
   }
 
---
+++ src/hooks/use-thunk.js
@@ -0,0 +1,19 @@
+import { useState, useCallback } from "react";
+import { useDispatch } from "react-redux";
+
+export function useThunk(thunk) {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const dispatch = useDispatch();
+
+  const runThunk = useCallback(() => {
+    // useCallback จะช่วยลดการสร้างฟังก์ชันใหม่ในทุกครั้งที่เกิดการเรียกใช้งานคอมโพเนนต์ของ React ที่มีการเปลี่ยนแปลงของพารามิเตอร์เข้ามาในการเรียกใช้งานฟังก์ชันนั้น ซึ่งส่งผลให้เกิดประสิทธิภาพการทำงานที่ดีขึ้น
+    setIsLoading(true);
+    dispatch(thunk())
+      .unwrap()
+      .catch((err) => setError(err))
+      .finally(() => setIsLoading(false));
+  }, [dispatch, thunk]);
+
+  return [runThunk, isLoading, error];
+}
